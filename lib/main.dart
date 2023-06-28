ef1d65ba13544def00408ea655215b7ce1f78569
00Adnan00 <adnanaljafarey638@gmail.com>
2023-06-28 21:47:43 +0200

    Added cloude firebase dependancy & init firebase in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,8 +9,10 @@ import 'package:raghwa/screens/home_screen.dart';
 import 'package:raghwa/screens/onboarding_screen.dart';
 import 'package:raghwa/widgets/no_internet.dart';
 
-void main() {
+void main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
 
   SystemChrome.setPreferredOrientations(const [
     DeviceOrientation.portraitUp,
